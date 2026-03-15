248524b55dd382daaeb3af7895194bdf8bd9261f
lawrencelonghi <lawlonghi@gmail.com>
2026-03-15 11:06:56 -0300

    update

--- client/src/components/GameSection.tsx
+++ client/src/components/GameSection.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { io, Socket} from 'socket.io-client';
+
 
 const GameSection = () => {
   return (
-    <div>GameSection</div>
+    <div>
+      game section
+    </div>
   )
 }
 
